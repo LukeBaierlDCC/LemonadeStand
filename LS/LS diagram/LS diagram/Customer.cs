0b744ce312d77ba5388aa1b6e3a9f1a09153491f
Luke <lukeest1984@gmail.com>
2019-02-04 14:17:39 -0600

    Added more code to Customer class to affect weather influence.

--- LS/LS diagram/LS diagram/Customer.cs
+++ LS/LS diagram/LS diagram/Customer.cs
@@ -62,5 +62,63 @@ namespace LS_diagram
                 chanceToBuy += (underPrice * 100);
             }
         }
+        public void RecipeInfluence(LemonadeStand lemonadeStand)
+        {
+            chanceToBuy += LemonInfluence(lemonadeStand.Recipe.lemonsToUse);
+            chanceToBuy += SugarInfluence(lemonadeStand.Recipe.sugarToUse);
+            chanceToBuy += IceInfluence(lemonadeStand.Recipe.iceToUse);
+        }
+        public double LemonInfluence(double lemons)
+        {
+            double influence;
+            if (lemons < 8)
+            {
+                influence = (lemons - 4) * 2;
+            }
+            else
+            {
+                influence = (7 - lemons) * 2;
+            }
+
+            return influence;
+        }
+        public double SugarInfluence(double sugar)
+        {
+            double influence;
+            if (sugar > 4)
+            {
+                influence = (4 - sugar);
+            }
+            else
+            {
+                influence = (sugar - 4);
+            }
+            return influence;
+        }
+        public double Iceinfluence(double ice)
+        {
+            double influence;
+            if (ice<6)
+            {
+                influence = (ice - 4) * 2;
+            }
+            else
+            {
+                influence = (5 - ice) * 2;
+            }
+            return influence;
+
+        }
+
+        public void DoesBuy(Day day, Random num)
+        {
+            double buyNumber = num.Next(0, 101);
+            if (buyNumber < chanceToBuy)
+            {
+                day.payingCustomers += 1;
+                day.cupCounter--;
+                day.pitcher++;
+            }
+        }
     }
 }
