81e2cc4f3d1d0409e0fe0849fa4e103dba471345
Luke <lukeest1984@gmail.com>
2019-02-04 08:51:29 -0600

    Added some code to Customer class.

--- LS/LS diagram/LS diagram/Customer.cs
+++ LS/LS diagram/LS diagram/Customer.cs
@@ -7,6 +7,53 @@ namespace LS_diagram
 {
     public class CustomerType
     {
-
+        public void Customer
+        {
+            public void Decide(Weather weather, Player player)
+            {
+                Random rnd = new Random();
+                int chance = rnd.Next(1, 6);
+                if(chance == 1)
+                {
+                    PriceFactor(player);
+                }
+                else if (chance == 2)
+                {
+                    WeatherFactor(weather, player);
+                }
+                else if (chance == 3)
+                {
+                    RecipeFactor(player);
+                }
+                else
+                {
+                    Purchase(player);
+                }
+            }
+            public void Purchase(Player player)
+            {
+                player.Pitcher.cups -= 1;
+                player.Wallet += player.Recipe.pricePerCup;
+            }
+            public void PriceFactor(Player player)
+            {
+                if(player.Recipe.pricePerCup > 1)
+                {
+                    Random rnd = new Random();
+                    int chance = rnd.Next(1, 100);
+                    if(chance > 90)
+                    {
+                        Purchase(player);
+                    }
+                }
+            }
+            public void WeatherFactor(Weather weather, Player player)
+            {
+                if (weather.realTemp < && weather.skyType == "cloudy" || weather.skyType == "rainy")
+                {
+                Random rnd = new Random();
+                }
+            }
+        }
     }
 }
