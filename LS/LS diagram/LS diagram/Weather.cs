7673dd6d8095d1adbe15c39f28dbaecdb1071271
Luke <lukeest1984@gmail.com>
2019-02-04 08:15:05 -0600

    Fixed several previous errors in Weather class.

--- LS/LS diagram/LS diagram/Weather.cs
+++ LS/LS diagram/LS diagram/Weather.cs
@@ -8,38 +8,51 @@ namespace LS_diagram
     public class Weather
     {
         private double Forecast;
-        private int SunnyDay;
-        private int RainyDay;
-        private int CloudyDay;
+        public int realTemp;
+        public int degrees;
+        public int GetActualWeather;
+        int type;
+        public string skyType;
+        List<string> sky = new List<string>(new string[] { "sunny", "cloudy", "partly-cloudy", "rainy" });
+        Random num = new Random();
 
-        public string GetForecast()
+        public int Temperature()
+        {
+            degrees = num.Next(50, 100);
+            return degrees;
+        }
+
+        public string WeatherType()
+        {
+            type = num.Next(0, 3);
+            skyType = sky[type];
+            return skyType;
+        }
+        private double GetForecast()
         {
             int predictTemp = Temperature();
             string predictType = WeatherType();
-            string forecast = "The predicted weather is: " + predictType + " and " + predictTemp + " degrees\n";
+            string forecast = "Today's forecast is: " + predictType + " and " + predictTemp + " degrees\n";
             Console.WriteLine(forecast);
-            string realWeather = ActualWeather();
-            return skyType;
-            }
+            string realWeather = GetActualWeather();
+            return realWeather;
         }
 
-        public string GetActualWeather()
+        public string GetActualWeather(Random random)
         {
-            string GetActualWeather;
-            int difference = NotFiniteNumberException.Next(-5,5);
+            string realWeather;
+            int difference = num.Next(-5, 5);
             realTemp = degrees + difference;
             if (realTemp % degrees == 5)
             {
-                ActualWeather();
-                realWeather = "Today's forecast is: " +  + " and " + realTemp + " degrees\n";
+                WeatherType();
+                realWeather = "Today's actual weather is: " + skyType + " and " + realTemp + " degrees\n";
             }
             else
             {
-                realWeather = "Today's forecast is: " +  + " and " + realTemp + " degrees\n";
+                realWeather = "Today's actual weather is: " + skyType + " and " + realTemp + " degrees\n";
             }
             return realWeather;
-            
         }
-
     }
 }
