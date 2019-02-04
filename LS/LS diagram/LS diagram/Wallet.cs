22c27bf4b05c061ebf65bae60ae3022848b0bfea
Luke <lukeest1984@gmail.com>
2019-02-04 15:57:26 -0600

    Added code to Wallet class.

--- LS/LS diagram/LS diagram/Wallet.cs
+++ LS/LS diagram/LS diagram/Wallet.cs
@@ -7,5 +7,32 @@ namespace LS_diagram
 {
     public class Wallet
     {
+        public double moneyInWallet = 10.0;
+        bool bankrupt;
+        double completeTransaction;
+        double CostOfProduct;
+        public void DisplayBalance()
+        {
+            Console.WriteLine("Your total budget is ${0}", moneyInWallet);
+        }
+
+        public bool checkIfBankrupt(double CostOfProduct)
+        {
+            if (moneyInWallet < CostOfProduct)
+            {
+                bankrupt = true;
+                Console.WriteLine("You're too low on funds.");
+                Console.ReadLine();
+            }
+            return bankrupt;
+
+        }
+        public double buyProduct(double costOfProduct)
+        {
+            double earnings;
+            double startingFunds = 10;
+            earnings = moneyInWallet - startingFunds;
+            Console.WriteLine("You made {0} this week. Play again and try to earn more.", earnings);
+        }
     }
 }
