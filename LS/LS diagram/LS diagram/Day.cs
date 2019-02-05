ff7f98fc686576f71a60f3a59614d5ebefce6791
Luke <lukeest1984@gmail.com>
2019-02-05 09:39:37 -0600

    Added more code to Day class. Far from finished with it.

--- LS/LS diagram/LS diagram/Day.cs
+++ LS/LS diagram/LS diagram/Day.cs
@@ -70,6 +70,47 @@ namespace LS_diagram
             lemonadeStand.inventory.lemons -= lemonadeStand.Recipe.lemonsToUse;
             lemonadeStand.inventory.sugar -= lemonadeStand.Recipe.sugarToUse;
         }
-        
+        public void DisplayDayResults(double actualTemperature, string weatherCondition, LemonadeStand lemonadeStand)
+        {
+            Console.WriteLine("Today's actual weather was " + actualTemperature + " and " + weatherCondition + ".");
+            Console.WriteLine("");
+            Console.WriteLine("You sold " + payingCustomers + " cups of lemonade to " + possibleCustomers + " customers.");
+            Console.WriteLine("");
+            Console.WriteLine("Your popularity has risen to " + lemonadeStand.popularity + " and your customer satisfaction is at " + lemonadeStand.customerSatisfaction + "%.");
+            Console.WriteLine("Press enter so we can get back to selling.");
+            Console.WriteLine("");
+            Console.WriteLine("A new day of selling lemonade.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                return;
+            }
+            else
+            {
+                DisplayDayResults(actualTemperature, weatherCondition, lemonadeStand, currentDay);
+            }
+        }
+        public void UpdateEndOfDayVariables(LemonadeStand lemonadeStand)
+        {
+            lemonadeStand.Inventory.Cups = cupCounter;
+            lemonadeStand.Money += payingCustomers * lemonadeStand.Recipe.Price;
+            lemonadeStand.Inventory.Ice = 0;
+            lemonadeStand.Revenue += (payingCustomers * lemonadeStand.Recipe.Price);
+        }
+        public void RunThroughCustomers(Inventory inventory, LemonadeStand lemonadeStand, StoreClass store, Day day, Random num)
+        {
+            for (double j = 0; j < possibleCustomers; j++)
+            {
+                if (j == 0 || pitcher == 10 || pitcher == 20 || pitcher == 30 || pitcher == 40 || pitcher == 50 || pitcher == 60 || pitcher == 70 || pitcher == 80 || pitcher == 90 || pitcher == 100)
+                {
+                    UpdateInventory(lemonadeStand);
+                    pitcher++;
+                }
+                if (cupCounter > 0 && inventory.lemons > lemonadeStand.Recipe.lemonsToUse && inventory.sugar > lemonadeStand.Recipe.sugarToUse && inventory.ice > lemonadeStand.Recipe.iceToUse)
+                {
+
+                }
+            }
+        }
     }
 }
