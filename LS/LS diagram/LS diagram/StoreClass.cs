1d10a706cc68a3cbda3a028b1e4828de6c2be99e
Luke <lukeest1984@gmail.com>
2019-02-04 21:13:30 -0600

    Added code to the StoreClass.

--- LS/LS diagram/LS diagram/StoreClass.cs
+++ LS/LS diagram/LS diagram/StoreClass.cs
@@ -49,9 +49,98 @@ namespace LS_diagram
             lemonsToBuy = amount;
             double newTotal = player.cash - totalcost;
             player.cash = newTotal;
-            Console.WriteLine("There is " + player.cash + " left in your wallet.");
+            Console.WriteLine("There's a total of " + player.cash + " left in your budget.");
 
         }
-                
+         
+        public void SelectionOfIce(Player player)
+        {
+            int amount = 0;
+
+            double totalcost = 0;
+            Console.WriteLine("How many icecubes are you looking to buy? ");
+            int userinput = Int32.Parse(Console.ReadLine());
+
+            switch (userinput)
+            {
+                case 1:
+                    amount = 100;
+                    totalcost = .40;
+                    break;
+                case 2:
+                    amount = 200;
+                    totalcost = .75;
+                    break;
+                case 3:
+                    amount = 300;
+                    totalcost = 1.10;
+                    break;
+            }
+            iceToBuy = amount;
+            double newTotal = player.cash - totalcost;
+            player.cash = newTotal;
+            Console.WriteLine("There's a total of " + player.cash + " left in your budget.");
+
+        }
+
+        public void SelectionOfSugar(Player player)
+        {
+            int amount = 0;
+
+            double totalcost = 0;
+            Console.WriteLine("How many spoons of sugar would you like to buy? ");
+            int userinput = Int32.Parse(Console.ReadLine());
+
+            switch (userinput)
+            {
+                case 1:
+                    amount = 4;
+                    totalcost = .25;
+                    break;
+                case 2:
+                    amount = 9;
+                    totalcost = .40;
+                    break;
+                case 3:
+                    amount = 12;
+                    totalcost = .90;
+                    break;
+            }
+
+            sugarToBuy = amount;
+            double newTotal = player.cash - totalcost;
+            player.cash = newTotal;
+            Console.WriteLine("There's a total of " + player.cash + " left in your budget.");
+        }
+
+        public void SelectionOfCups(Player player)
+        {
+            int amount = 0;
+
+            double totalcost = 0;
+            Console.WriteLine("How many cups are you planning to buy? ");
+            int userinput = Int32.Parse(Console.ReadLine());
+
+            switch (userinput)
+            {
+                case 1:
+                    amount = 30;
+                    totalcost = .75;
+                    break;
+                case 2:
+                    amount = 60;
+                    totalcost = 1.45;
+                    break;
+                case 3:
+                    amount = 120;
+                    totalcost = 2.80;
+                    break;
+            }
+
+            cups = amount;
+            double newTotal = player.cash - totalcost;
+            player.cash = newTotal;
+            Console.WriteLine("There's a total of " + player.cash + " left in your budget.");
+        }
     }
 }
